ef2ddab17b0c27a5cf46a872d2265ce811f75f5e
alekseiveprev <alekseiveprev@gmail.com>
2023-04-01 20:14:29 +0300

    init: start Social Network task

---
+++ src/components/Profile.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Profile = () => {
+
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+
+
+            Main content
+        </div>
+
+    )
+}
+
+export default Profile;
