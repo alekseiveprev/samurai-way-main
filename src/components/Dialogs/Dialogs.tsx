e48717df4b6be4e4d8f3b6757bdad4d6ce4446e3
alekseiveprev <alekseiveprev@gmail.com>
2023-04-21 19:04:19 +0300

    feat: Route to App.tsx

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './Dialogs.module.css';
+
+type DialogsPropsType = {
+
+}
+const Dialogs = (props:  any) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+
+    )
+}
+
+
+export default Dialogs;
---
+++ src/components/Profile/MyPost/Post/Post.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './Post.module.css';
+
+type PostPropsType ={
+    message?: string
+    likesCount?: string
+}
+const Post = (props: PostPropsType) => {
+
+
+    return (
+        <div className={s.item}>
+            <img
+                src="https://mcswebsites.blob.core.windows.net/1013/Event_7891/gallery/avatar_the_way_of_water_ver16.jpg"
+                alt=""/>
+            {props.message}
+            <div>
+                <span>Like </span>{props.likesCount}
+            </div>
+        </div>
+
+    )
+}
+
+export default Post;
