397716d8af6bb8cb32f5b8179d3c1426e436cf2b
alekseiveprev <72622572+alekseiveprev@users.noreply.github.com>
2023-10-30 08:21:04 +0000

    feat: 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import s from './Dialogs.module.css';
+import s from "./Dialogs.module.css";
 
-type DialogsPropsType = {
-
-}
-const Dialogs = (props:  any) => {
-    return (
-        <div>
-            Dialogs
-        </div>
-
-    )
-}
-
+type DialogsPropsType = {};
+const Dialogs = (props: any) => {
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItem}>
+        <div className={s.dialog + " " + s.active}>Dimych</div>
+        <div className={s.dialog}>Ivan</div>
+        <div className={s.dialog}>Alex</div>
+        <div className={s.dialog}>Sveta</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
